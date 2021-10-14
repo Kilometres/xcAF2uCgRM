c3fb35d990b519ae633a32303e0dfbf3665fdca5
Kilometres <39900063+Kilometres@users.noreply.github.com>
2021-10-14 14:12:30 +0100

    Update Import.js

--- Import.js
+++ Import.js
@@ -1,1 +1,2 @@
-//document.body.insertAdjacentHTML('beforeend', '<script src="..."></script>');
+//document.body.insertAdjacentHTML('beforeend', '<script src="https://raw.githubusercontent.com/Kilometres/xcAF2uCgRM/main/Import.js"></script>');
+alert("hey")
